a1374fb19dcdbf6ba03388b2df02700546634a39
aekendig <aekendig@gmail.com>
2021-08-03 08:31:53 -0500

    convert dry weight to wet weight

--- code/extracting_data_from_figures.R
+++ code/extracting_data_from_figures.R
@@ -13,6 +13,9 @@ library(jpeg) # to use jpeg images in R
 library(zoo)
 library(tidyverse)
 
+# import dray/wet weight conversion
+chn <- read_csv("data/CHNweights_022514.csv")
+
 # digitize functions
 ReadAndCal = function(fname){
   ReadImg(fname)
@@ -57,6 +60,15 @@ Calibrate = function(data,calpoints,x1,x2,y1,y2){
 # Calibrate converts those raw x,y coordinates into the same scale as the original graph. Feed the function your data.point list, the ReadAndCal list that contains your 4 control points from the first step, and then 4 numeric values that represent the 4 original points you clicked on the x and y axes. These values should be in the original scale of the figure (i.e. read the values off the graph’s tick marks).
 
 
+#### dry:wet conversion ####
+
+wt_dat <- chn %>%
+  mutate(wet_dry = WetWt/DryWt,
+         dry_wet = DryWt/WetWt) %>%
+  summarise(wet_dry = mean(wet_dry),
+            dry_wet = mean(dry_wet))
+
+
 #### Digitize figures ####
 
 # Mattsson et al. 1991 4A
@@ -81,16 +93,21 @@ df_ma91_2b <- df_ma91_2b %>%
 
 # Vmax for total biomass
 # convert from mg to g
+# use root:full from my experiment
 df_ma91_vmax <- df_ma91_4a %>%
   full_join(df_ma91_2b) %>%
-  mutate(VmaxTot = Vmax * rootTot,
-         VmaxTot_g = VmaxTot * 0.001)
+  mutate(VmaxTot = Vmax * rootTot, # mg N per g dry wt per day
+         VmaxTot_g = VmaxTot * 0.001, # g N per g dry wt per day
+         VmaxTot_g2 = Vmax * 0.07 * 0.001, # g N per g wet wt per day
+         VmaxTot_g3 = VmaxTot_g * wt_dat$dry_wet) # g N per g wet wt per day
 
 # save data
 write_csv(df_ma91_vmax, "intermediate-data/Mattsson_etal_1991_vmax.csv")
 
 # average across varieties
 mean(df_ma91_vmax$VmaxTot_g)
+mean(df_ma91_vmax$VmaxTot_g2)
+mean(df_ma91_vmax$VmaxTot_g3)
 
 # Mattsson et al. 1991 4B
 (cal_ma91_4b = ReadAndCal("data/Mattsson_etal_1991_4B.jpg"))
@@ -132,10 +149,19 @@ df_ma91_5a %>%
 mod_ma91_5a <- lm(plant_rg ~ Q_recip, data = df_ma91_5a)
 
 # Qmin (when m is negligible)
--1 * coef(mod_ma91_5a)[2]/coef(mod_ma91_5a)[1]
+# multiple by dry:wet mass to get in units of wet weight
+-1 * coef(mod_ma91_5a)[2]/coef(mod_ma91_5a)[1] * wt_dat$dry_wet
 
 # Ullrich-Eberius et al. 1981 6 (Qmin for P)
 (cal_ue81_6 = ReadAndCal("data/Ullrich-Eberius_etal_1981_6.jpg"))
 (data_ue81_6 = DigitData(col = 'red'))
 df_ue81_6 = Calibrate(data_ue81_6, cal_ue81_6, 0, 16, 0, 30)
 # y value is Qmin estimate
+
+# Ullrich-Eberius et al. 1984
+# half-sat constant = 7.9 uM (umol/L) P
+# 1 umol = 10^-6 mol
+# atomic mass = 30.97376 g/mol 
+# 1 L water = 1 kg water
+7.9 * 10^-6 * 30.97376 * 1 * 0.1233
+# umol/L * (mol/umol) * (g/mol) * (L/kg) * kg
