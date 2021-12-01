1b046006a8ac74043aba185edf7ea602ca5e39e4
aekendig <aekendig@gmail.com>
2021-12-01 07:40:05 -0600

    revise virus functions to include all nutrients

--- code/ode_fit_parameter_estimation.R
+++ code/ode_fit_parameter_estimation.R
@@ -1,9 +1,5 @@
 ## Goal: estimate plant and virus growth rates
 
-#### start here:
-# delete non "full" functions and remove "full" word
-# fit pathogen parameters
-
 
 #### set up ####
 
@@ -191,31 +187,6 @@ V0 <- 100000
 plant_model = function (t, yy, parms) { 
   
   # supply rates
-  g = parms[1];
-  a_n = ifelse(length(parms) > 1, parms[2], a_n);
-  a_p = ifelse(length(parms) > 2, parms[3], a_p);
-  m = ifelse(length(parms) == 4, parms[4], m);
-
-  # set initial values
-  R_n = yy[1];
-  R_p = yy[2];
-  Q_n = yy[3];
-  Q_p = yy[4];
-  H = yy[5];
- 
-  # model
-  dR_n = a_n - (u_n * R_n * H) / (R_n + k_n);
-  dR_p = a_p - (u_p * R_p * H) / (R_p + k_p);
-  dQ_n = (u_n * R_n) / (R_n + k_n) - min((1 - Qmin_n / Q_n), (1 - Qmin_p / Q_p)) * g * Q_n
-  dQ_p = (u_p * R_p) / (R_p + k_p) - min((1 - Qmin_n / Q_n), (1 - Qmin_p / Q_p)) * g * Q_p
-  dH = min((1 - Qmin_n / Q_n), (1 - Qmin_p / Q_p)) * g * H - m * H
-  
-  return(list(c(dR_n, dR_p, dQ_n, dQ_p, dH)))
-}
-
-plant_model_full = function (t, yy, parms) { 
-  
-  # supply rates
   m = parms[1];
   g = ifelse(length(parms) == 2, parms[2], g);
   
@@ -294,72 +265,13 @@ mock <- dat5 %>%
            fct_relevel("low", "+N", "+P"))
 
 # wrapper function
-plant_wrapper <- function(g, m){
-  
-  out_low <- ode(c(R_n = R0_n_lo, R_p = R0_p_lo, Q_n = Q0_n, Q_p = Q0_p, H = H0), 
-                 times, plant_model, c(g = g, a_n = a_n_lo, a_p = a_p_lo, m = m)) %>%
-    as_tibble() %>%
-    mutate(nutrient = "low",
-           across(!nutrient, as.double),
-           nutrient = as.character(nutrient))
-  
-  out_N <- ode(c(R_n = R0_n_hi, R_p = R0_p_lo, Q_n = Q0_n, Q_p = Q0_p, H = H0),
-               times, plant_model, c(g = g, a_n = a_n_hi, a_p = a_p_lo, m = m)) %>%
-    as_tibble() %>%
-    mutate(nutrient = "+N",
-           across(!nutrient, as.double),
-           nutrient = as.character(nutrient))
-  
-  out_P <- ode(c(R_n = R0_n_lo, R_p = R0_p_hi, Q_n = Q0_n, Q_p = Q0_p, H = H0),
-               times, plant_model, c(g = g, a_n = a_n_lo, a_p = a_p_hi, m = m)) %>%
-    as_tibble() %>%
-    mutate(nutrient = "+P",
-           across(!nutrient, as.double),
-           nutrient = as.character(nutrient))
-  
-  out_NP <- ode(c(R_n = R0_n_hi, R_p = R0_p_hi, Q_n = Q0_n, Q_p = Q0_p, H = H0),
-                times, plant_model, c(g = g, a_n = a_n_hi, a_p = a_p_hi, m = m)) %>%
-    as_tibble() %>%
-    mutate(nutrient = "+N+P",
-           across(!nutrient, as.double),
-           nutrient = as.character(nutrient))
-  
-  out <- out_low %>%
-    full_join(out_N) %>%
-    full_join(out_P) %>%
-    full_join(out_NP) %>%
-    pivot_longer(cols = c(R_n, R_p, Q_n, Q_p, H),
-                 names_to = "variable",
-                 values_to = "value") %>%
-    mutate(nutrient = fct_relevel(nutrient, "low", "+N", "+P"))
-  
-  # use to visualize all values
-  # ggplot(out, aes(x = time, y = value, color = nutrient)) +
-  #   geom_line() +
-  #   stat_summary(data = mock, geom = "errorbar", width = 0, fun.data = "mean_se") +
-  #   stat_summary(data = mock, geom = "point", size = 2, fun = "mean") +
-  #   facet_wrap(~ variable, scales = "free")
-  # equilibrium well before 1000
-  
-  mock_mean <- mock %>%
-    group_by(nutrient) %>%
-    summarize(value = mean(value)) %>%
-    ungroup()
-
-  ggplot(filter(out, variable == "H"), aes(x = time, y = value)) +
-    geom_line() +
-    geom_hline(data = mock_mean, aes(yintercept = value), linetype = "dashed") +
-    geom_point(data = mock) +
-    facet_wrap(~ nutrient, scales = "free")
-}
-
-plant_wrapper_full <- function(m, g){
+plant_wrapper <- function(m, g){
   
   out <- ode(c(R_n_low = R0_n_lo, R_p_low = R0_p_lo, Q_n_low = Q0_n, Q_p_low = Q0_p, H_low = H0,
                R_n_n = R0_n_hi, R_p_n = R0_p_lo, Q_n_n = Q0_n, Q_p_n = Q0_p, H_n = H0,
                R_n_p = R0_n_lo, R_p_p = R0_p_hi, Q_n_p = Q0_n, Q_p_p = Q0_p, H_p = H0,
                R_n_np = R0_n_hi, R_p_np = R0_p_hi, Q_n_np = Q0_n, Q_p_np = Q0_p, H_np = H0),
-             times, plant_model_full, c(m = m, g = g)) %>%
+             times, plant_model, c(m = m, g = g)) %>%
     as_tibble() %>%
     mutate(across(everything(), as.double)) %>%
     pivot_longer(cols = -time,
@@ -403,7 +315,7 @@ manipulate(plot(1:5, cex=size), size = slider(0.5,10,step=0.5))
 # time 
 times <- seq(0, max(dat5$dpp)*2, length.out = 100)
 
-manipulate(plant_wrapper_full(m, g), m = slider(0, 0.1), g = slider(0, 1))
+manipulate(plant_wrapper(m, g), m = slider(0, 0.1), g = slider(0, 1))
 # g ~ 0.144
 # m ~ 0.004
 
@@ -429,9 +341,8 @@ plant_cost <- function(input_plant){
                   R_n_n = R0_n_hi, R_p_n = R0_p_lo, Q_n_n = Q0_n, Q_p_n = Q0_p, H_n = H0,
                   R_n_p = R0_n_lo, R_p_p = R0_p_hi, Q_n_p = Q0_n, Q_p_p = Q0_p, H_p = H0,
                   R_n_np = R0_n_hi, R_p_np = R0_p_hi, Q_n_np = Q0_n, Q_p_np = Q0_p, H_np = H0),
-            times = times, func = plant_model_full, parms = c(m = m))
+            times = times, func = plant_model, parms = c(m = m))
   return(modCost(model = out[ , c("time", "H_low", "H_n", "H_p", "H_np")], obs = mock_fit, y = "value"))   
-  # return(out[ , c("time", "H")]) #for troubleshooting
 }
 
 
@@ -457,8 +368,8 @@ m <- fit_plant$par[1]; # 0.007
 times <- seq(0, max(dat5$dpp)*2, length.out = 100)
 
 # figure
-plant_wrapper_full(m, g) +
-  # fig_theme +
+plant_wrapper(m, g) +
+  fig_theme +
   labs(x = "Time (days)", y = "Plant biomass (g)")
 
 
@@ -470,28 +381,71 @@ plant_wrapper_full(m, g) +
 virus_model = function (t, yy, parms) { 
   
   # unknown parameters and supply rates
-  r = parms[1];
-  a_n = ifelse(length(parms) > 1, parms[2], a_n);
-  a_p = ifelse(length(parms) > 2, parms[3], a_p);
-  c = ifelse(length(parms) == 4, parms[4], c);
+  c = parms[1];
+  r = ifelse(length(parms) == 2, parms[2], r);
   
   # set initial values
-  R_n = yy[1];
-  R_p = yy[2];
-  Q_n = yy[3];
-  Q_p = yy[4];
-  H = yy[5];
-  V = yy[6];
+  R_n_low = yy[1];
+  R_p_low = yy[2];
+  Q_n_low = yy[3];
+  Q_p_low = yy[4];
+  H_low = yy[5];
+  V_low = yy[6];
+  
+  R_n_n = yy[7];
+  R_p_n = yy[8];
+  Q_n_n = yy[9];
+  Q_p_n = yy[10];
+  H_n = yy[11];
+  V_n = yy[12];
+  
+  R_n_p = yy[13];
+  R_p_p = yy[14];
+  Q_n_p = yy[15];
+  Q_p_p = yy[16];
+  H_p = yy[17]
+  V_p = yy[18];
+  
+  R_n_np = yy[19];
+  R_p_np = yy[20];
+  Q_n_np = yy[21];
+  Q_p_np = yy[22];
+  H_np = yy[23];
+  V_np = yy[24];
   
   # model
-  dR_n = a_n - (u_n * R_n * H) / (R_n + k_n);
-  dR_p = a_p - (u_p * R_p * H) / (R_p + k_p);
-  dQ_n = (u_n * R_n) / (R_n + k_n) - min((1 - Qmin_n / Q_n), (1 - Qmin_p / Q_p)) * g * Q_n - min((1 - q_n / Q_n), (1 - q_p / Q_p)) * z_n * r * V
-  dQ_p = (u_p * R_p) / (R_p + k_p) - min((1 - Qmin_n / Q_n), (1 - Qmin_p / Q_p)) * g * Q_p - min((1 - q_n / Q_n), (1 - q_p / Q_p)) * z_p * r * V
-  dH = min((1 - Qmin_n / Q_n), (1 - Qmin_p / Q_p)) * g * H - m * H
-  dV = min((1 - q_n / Q_n), (1 - q_p / Q_p)) * r * V - c * V
+  dR_n_low = a_n_lo - (u_n * R_n_low * H_low) / (R_n_low + k_n);
+  dR_p_low = a_p_lo - (u_p * R_p_low * H_low) / (R_p_low + k_p);
+  dQ_n_low = (u_n * R_n_low) / (R_n_low + k_n) - min((1 - Qmin_n / Q_n_low), (1 - Qmin_p / Q_p_low)) * g * Q_n_low - min((1 - q_n / Q_n_low), (1 - q_p / Q_p_low)) * z_n * r * V_low
+  dQ_p_low = (u_p * R_p_low) / (R_p_low + k_p) - min((1 - Qmin_n / Q_n_low), (1 - Qmin_p / Q_p_low)) * g * Q_p_low - min((1 - q_n / Q_n_low), (1 - q_p / Q_p_low)) * z_p * r * V_low
+  dH_low = min((1 - Qmin_n / Q_n_low), (1 - Qmin_p / Q_p_low)) * g * H_low - m * H_low
+  dV_low = min((1 - q_n / Q_n_low), (1 - q_p / Q_p_low)) * r * V_low - c * V_low
   
-  return(list(c(dR_n, dR_p, dQ_n, dQ_p, dH, dV)))
+  dR_n_n = a_n_hi - (u_n * R_n_n * H_n) / (R_n_n + k_n);
+  dR_p_n = a_p_lo - (u_p * R_p_n * H_n) / (R_p_n + k_p);
+  dQ_n_n = (u_n * R_n_n) / (R_n_n + k_n) - min((1 - Qmin_n / Q_n_n), (1 - Qmin_p / Q_p_n)) * g * Q_n_n - min((1 - q_n / Q_n_n), (1 - q_p / Q_p_n)) * z_n * r * V_n
+  dQ_p_n = (u_p * R_p_n) / (R_p_n + k_p) - min((1 - Qmin_n / Q_n_n), (1 - Qmin_p / Q_p_n)) * g * Q_p_n - min((1 - q_n / Q_n_n), (1 - q_p / Q_p_n)) * z_p * r * V_n
+  dH_n = min((1 - Qmin_n / Q_n_n), (1 - Qmin_p / Q_p_n)) * g * H_n - m * H_n
+  dV_n = min((1 - q_n / Q_n_n), (1 - q_p / Q_p_n)) * r * V_n - c * V_n
+  
+  dR_n_p = a_n_lo - (u_n * R_n_p * H_p) / (R_n_p + k_n);
+  dR_p_p = a_p_hi - (u_p * R_p_p * H_p) / (R_p_p + k_p);
+  dQ_n_p = (u_n * R_n_p) / (R_n_p + k_n) - min((1 - Qmin_n / Q_n_p), (1 - Qmin_p / Q_p_p)) * g * Q_n_p - min((1 - q_n / Q_n_p), (1 - q_p / Q_p_p)) * z_n * r * V_p
+  dQ_p_p = (u_p * R_p_p) / (R_p_p + k_p) - min((1 - Qmin_n / Q_n_p), (1 - Qmin_p / Q_p_p)) * g * Q_p_p - min((1 - q_n / Q_n_p), (1 - q_p / Q_p_p)) * z_p * r * V_p
+  dH_p = min((1 - Qmin_n / Q_n_p), (1 - Qmin_p / Q_p_p)) * g * H_p - m * H_p
+  dV_p = min((1 - q_n / Q_n_p), (1 - q_p / Q_p_p)) * r * V_p - c * V_p
+  
+  dR_n_np = a_n_hi - (u_n * R_n_np * H_np) / (R_n_np + k_n);
+  dR_p_np = a_p_hi - (u_p * R_p_np * H_np) / (R_p_np + k_p);
+  dQ_n_np = (u_n * R_n_np) / (R_n_np + k_n) - min((1 - Qmin_n / Q_n_np), (1 - Qmin_p / Q_p_np)) * g * Q_n_np - min((1 - q_n / Q_n_np), (1 - q_p / Q_p_np)) * z_n * r * V_np
+  dQ_p_np = (u_p * R_p_np) / (R_p_np + k_p) - min((1 - Qmin_n / Q_n_np), (1 - Qmin_p / Q_p_np)) * g * Q_p_np - min((1 - q_n / Q_n_np), (1 - q_p / Q_p_np)) * z_p * r * V_np
+  dH_np = min((1 - Qmin_n / Q_n_np), (1 - Qmin_p / Q_p_np)) * g * H_np - m * H_np
+  dV_np = min((1 - q_n / Q_n_np), (1 - q_p / Q_p_np)) * r * V_np - c * V_np
+  
+  return(list(c(dR_n_low, dR_p_low, dQ_n_low, dQ_p_low, dH_low, dV_low,
+                dR_n_n, dR_p_n, dQ_n_n, dQ_p_n, dH_n, dV_n,
+                dR_n_p, dR_p_p, dQ_n_p, dQ_p_p, dH_p, dV_p,
+                dR_n_np, dR_p_np, dQ_n_np, dQ_p_np, dH_np, dV_np)))
 }
 
 
@@ -500,7 +454,11 @@ virus_model = function (t, yy, parms) {
 # data
 pav <- dat5 %>%
   filter(inoc == "PAV") %>%
-  mutate(variable = "V",
+  mutate(variable = case_when(nutrient == "low" ~ "V_low",
+                              nutrient == "N" ~ "V_n",
+                              nutrient == "P" ~ "V_p",
+                              nutrient == "N+P" ~ "V_np"),
+         variable2 = "V",
          value = conc_PAV,
          time = dpi,
          nutrient = fct_recode(nutrient, "+N" = "N",
@@ -510,7 +468,11 @@ pav <- dat5 %>%
 
 rpv <- dat5 %>%
   filter(inoc == "RPV") %>%
-  mutate(variable = "V",
+  mutate(variable = case_when(nutrient == "low" ~ "V_low",
+                              nutrient == "N" ~ "V_n",
+                              nutrient == "P" ~ "V_p",
+                              nutrient == "N+P" ~ "V_np"),
+         variable2 = "V",
          value = conc_RPV,
          time = dpi,
          nutrient = fct_recode(nutrient, "+N" = "N",
@@ -526,47 +488,66 @@ min(pav$conc_PAV)
 min(rpv$conc_RPV)
 
 # initial plant values
-virus_init_fun <- function(N_level, P_level){
+virus_init_fun <- function(plant_time){
   
-  # initial resource values
-  if(N_level == "low"){
-    R0_n <- R0_n_lo
-    a_n <- a_n_lo
-  }else{
-    R0_n <- R0_n_hi
-    a_n <- a_n_hi
-  }
-  
-  if(P_level == "low"){
-    R0_p <- R0_p_lo
-    a_p <- a_p_lo
-  }else{
-    R0_p <- R0_p_hi
-    a_p <- a_p_hi
-  }
-  
-  plant_init <- ode(y = c(R_n = R0_n, R_p = R0_p, Q_n = Q0_n, Q_p = Q0_p, H = H0), 
-                    times = seq(0, 11, length.out = 100), func = plant_model, parms = c(g, a_n, a_p)) %>%
+  # simulate plant growth
+  plant_init <- ode(c(R_n_low = R0_n_lo, R_p_low = R0_p_lo, Q_n_low = Q0_n, Q_p_low = Q0_p, H_low = H0,
+                      R_n_n = R0_n_hi, R_p_n = R0_p_lo, Q_n_n = Q0_n, Q_p_n = Q0_p, H_n = H0,
+                      R_n_p = R0_n_lo, R_p_p = R0_p_hi, Q_n_p = Q0_n, Q_p_p = Q0_p, H_p = H0,
+                      R_n_np = R0_n_hi, R_p_np = R0_p_hi, Q_n_np = Q0_n, Q_p_np = Q0_p, H_np = H0),
+                    times = seq(0, plant_time, length.out = 100), plant_model, c(m, g)) %>%
     as_tibble() %>%
     mutate(across(everything(), as.double)) %>%
-    filter(time == 11)
+    filter(time == plant_time)
   
-  y0_virus <- c(R_n = pull(plant_init, R_n), 
-                R_p = pull(plant_init, R_p), 
-                Q_n = pull(plant_init, Q_n), 
-                Q_p = pull(plant_init, Q_p), 
-                H = pull(plant_init, H), 
-                V = V0)
+  y0_virus <- c(R_n_low = pull(plant_init, R_n_low), 
+                R_p_low = pull(plant_init, R_p_low), 
+                Q_n_low = pull(plant_init, Q_n_low), 
+                Q_p_low = pull(plant_init, Q_p_low), 
+                H_low = pull(plant_init, H_low), 
+                V_low = V0,
+                R_n_n = pull(plant_init, R_n_n), 
+                R_p_n = pull(plant_init, R_p_n), 
+                Q_n_n = pull(plant_init, Q_n_n), 
+                Q_p_n = pull(plant_init, Q_p_n), 
+                H_n = pull(plant_init, H_n), 
+                V_n = V0,
+                R_n_p = pull(plant_init, R_n_p), 
+                R_p_p = pull(plant_init, R_p_p), 
+                Q_n_p = pull(plant_init, Q_n_p), 
+                Q_p_p = pull(plant_init, Q_p_p), 
+                H_p = pull(plant_init, H_p), 
+                V_p = V0,
+                R_n_np = pull(plant_init, R_n_np), 
+                R_p_np = pull(plant_init, R_p_np), 
+                Q_n_np = pull(plant_init, Q_n_np), 
+                Q_p_np = pull(plant_init, Q_p_np), 
+                H_np = pull(plant_init, H_np), 
+                V_np = V0)
   
   return(y0_virus)
 }
- 
-
-# initiate slider for ggplot
-manipulate(plot(1:5, cex=size), size = slider(0.5,10,step=0.5))
 
 # wrapper function
-virus_wrapper <- function(r, c, species){
+virus_wrapper <- function(c, r, species, plant_time){
+  
+  out <- ode(virus_init_fun(plant_time),
+             times, virus_model, c(c = c, r = r)) %>%
+    as_tibble() %>%
+    mutate(across(everything(), as.double)) %>%
+    pivot_longer(cols = -time,
+                 names_to = "variable",
+                 values_to = "value") %>%
+    mutate(nutrient = case_when(str_ends(variable, "low") == T ~ "low",
+                                str_ends(variable, "np") == T ~ "+N+P",
+                                str_ends(variable, "n") == T ~ "+N",
+                                str_ends(variable, "p") == T ~ "+P"),
+           variable2 = case_when(str_starts(variable, "R_n") == T ~ "R_n",
+                                 str_starts(variable, "R_p") == T ~ "R_p",
+                                 str_starts(variable, "Q_n") == T ~ "Q_n",
+                                 str_starts(variable, "Q_p") == T ~ "Q_p",
+                                 str_starts(variable, "H") == T ~ "H",
+                                 str_starts(variable, "V") == T ~ "V"))
   
   if(species == "PAV"){
     virus <- pav
@@ -574,55 +555,19 @@ virus_wrapper <- function(r, c, species){
     virus <- rpv
   }
   
-  out_low <- ode(virus_init_fun("low", "low"), 
-                 times, virus_model, c(r = r, a_n = a_n_lo, a_p = a_p_lo, c = c)) %>%
-    as_tibble() %>%
-    mutate(nutrient = "low",
-           across(!nutrient, as.double),
-           nutrient = as.character(nutrient))
-  
-  out_N <- ode(virus_init_fun("high", "low"), 
-               times, virus_model, c(r = r, a_n = a_n_hi, a_p = a_p_lo, c = c)) %>%
-    as_tibble() %>%
-    mutate(nutrient = "+N",
-           across(!nutrient, as.double),
-           nutrient = as.character(nutrient))
-  
-  out_P <- ode(virus_init_fun("low", "high"), 
-               times, virus_model, c(r = r, a_n = a_n_lo, a_p = a_p_hi, c = c)) %>%
-    as_tibble() %>%
-    mutate(nutrient = "+P",
-           across(!nutrient, as.double),
-           nutrient = as.character(nutrient))
-  
-  out_NP <- ode(virus_init_fun("high", "high"), 
-                times, virus_model, c(r = r, a_n = a_n_hi, a_p = a_p_hi, c = c)) %>%
-    as_tibble() %>%
-    mutate(nutrient = "+N+P",
-           across(!nutrient, as.double),
-           nutrient = as.character(nutrient))
-  
-  out <- out_low %>%
-    full_join(out_N) %>%
-    full_join(out_P) %>%
-    full_join(out_NP) %>%
-    pivot_longer(cols = c(R_n, R_p, Q_n, Q_p, H, V),
-                 names_to = "variable",
-                 values_to = "value")
-  
   # use to visualize all variables
   # ggplot(out, aes(x = time, y = value, color = nutrient)) +
   #   geom_line() +
   #   stat_summary(data = virus, geom = "errorbar", width = 0, fun.data = "mean_se") +
   #   stat_summary(data = virus, geom = "point", size = 2, fun = "mean") +
-  #   facet_wrap(~ variable, scales = "free")
-
+  #   facet_wrap(~ variable2, scales = "free")
+  
   virus_mean <- virus %>%
     group_by(nutrient) %>%
     summarize(value = mean(value)) %>%
     ungroup()
 
-  ggplot(filter(out, variable == "V"), aes(x = time, y = value)) +
+  ggplot(filter(out, variable2 == "V"), aes(x = time, y = value)) +
     geom_line() +
     geom_hline(data = virus_mean, aes(yintercept = value), linetype = "dashed") +
     stat_summary(data = virus, geom = "errorbar", width = 0, fun.data = "mean_se") +
@@ -630,17 +575,19 @@ virus_wrapper <- function(r, c, species){
     facet_wrap(~ nutrient, scales = "free")
 }
 
+# initiate slider for ggplot
+manipulate(plot(1:5, cex=size), size = slider(0.5,10,step=0.5))
+
 # time 
-times <- seq(0, max(dat5$dpi) * 2, length.out = 100)
+times <- seq(0, max(dat5$dpi) * 25, length.out = 100)
 
 # PAV
 z_n <- z_nb
 z_p <- z_pb
-manipulate(virus_wrapper(r, c, species = "PAV"), r = slider(g, 5), c = slider(m, 1))
-# aiming to fit N+P with others close
+V0 <- 5e5
+manipulate(virus_wrapper(c, r, species = "PAV", plant_time = 11), c = slider(0, 0.1), r = slider(g, 5))
 # r ~ 0.3
 # c ~ 0.02
-# only use through dpi = 19
 
 # RPV
 z_n <- z_nc
