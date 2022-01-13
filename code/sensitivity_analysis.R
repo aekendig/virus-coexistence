49c7e7293bf8abc4655635b6cd6f5afe94edbb90
aekendig <aekendig@gmail.com>
2022-01-13 08:02:10 -0600

    evaluate q-z interaction

--- code/sensitivity_analysis.R
+++ code/sensitivity_analysis.R
@@ -229,9 +229,9 @@ pav_inv_q_n %>%
 
 #### minimum nutrient concentration/nutrient content of virus ####
 
-# values for low nutrient supply
+# values for z and q
 z_nc_vals <- 10^seq(-18, 0, length.out = 10)
-q_nc_vals2 <- 10^(-3:6)
+q_nc_vals2 <- 10^(-4:5) # simulations crash for low q's
 
 # data frame
 qz_nc_in <- tibble(param_foc1 = "z_nc",
@@ -271,8 +271,8 @@ pav_inv_qz_nc %>%
 
 # time series to clarify why
 params_qz_nc <- params_def2
-params_qz_nc["z_nc"] <- z_nc_vals[7]
-params_qz_nc["q_nc"] <- q_nc_vals2[2]
+params_qz_nc["z_nc"] <- z_nc_vals[1]
+params_qz_nc["q_nc"] <- q_nc_vals2[1]
 
 sim_qz_nc <- virus2_model_sim(params_qz_nc, "RPV", V0_b = V0, V0_c = V0, 
                               plant_time = 11, res_time = 12, 
@@ -296,316 +296,93 @@ plant_fig_fun(sim_qz_nc, params_qz_nc, -2e-3, -5e-4)
 # this is an interaction between q and z -- doesn't happen with increased q alone
 # only occurs once z is set to 1e-6, this is probably when virus drawdown of Q
 # starts to have a large impact. Why does it equilibrate at q_nc though?
-#### start here ####
 
 
-#### older code ####
+#### q time series ####
 
-#### initial simulation settings ####
+# change q value
+params_q_nc_lo <- params_def2
+params_q_nc_lo["q_nc"] <- 1.1e-5
+params_q_nc_hi <- params_def2
+params_q_nc_hi["q_nc"] <- 1.1e-1
 
-n <- 10
+# run simulations
+sim_q_nc_lo <- virus2_model_sim(params_q_nc_lo, "RPV", V0_b = 0, V0_c = V0, 
+                                plant_time = 11, res_time = 12, 
+                                inv_time = 100-11-12) %>%
+  virus2_model_format(params_q_nc_lo)
 
-param_df <- tibble(param_foc = rep(c("q_nb", "q_pb", "q_nc", "q_pc", 
-                                     "z_nb", "z_pb", "z_nc", "z_pc", 
-                                     "c_b", "c_c", 
-                                     "r_b", "r_c"), each = n), 
-                   param_val = c(rep(seq(1e-6, 1e-2, length.out = n), 4),
-                                 rep(seq(1e-20, 1e-17, length.out = n), 4),
-                                 rep(seq(1e-4, 1e-2, length.out = n), 2),
-                                 rep(seq(1e-3, 1, length.out = n), 2)))
+sim_q_nc_df <- virus2_model_sim(params_def2, "RPV", V0_b = 0, V0_c = V0, 
+                                plant_time = 11, res_time = 12, 
+                                inv_time = 100-11-12) %>%
+  virus2_model_format(params_def2)
 
-#### PAV initial simulations ####
+sim_q_nc_hi <- virus2_model_sim(params_q_nc_hi, "RPV", V0_b = 0, V0_c = V0, 
+                                plant_time = 11, res_time = 12, 
+                                inv_time = 100-11-12) %>%
+  virus2_model_format(params_q_nc_hi)
 
-# alone
-V0_b <- V0_init
-V0_c <- 0
-pav_only_params <- param_df %>%
-  mutate(first_virus = "RPV") %>%
-  mutate(pav_conc = pmap(., function(param_foc, param_val, first_virus) param_fun(params_def, param_foc, param_val, first_virus, max_days))) %>%
-  unnest(cols = c(pav_conc)) %>%
-  mutate(nutrient = fct_relevel(nutrient, "low", "+N", "+P"))
+# combine simulations
+sim_q_nc <- sim_q_nc_lo %>%
+  mutate(q_nc = "low (1.1e-5)") %>%
+  full_join(sim_q_nc_df %>%
+              mutate(q_nc = "med (1.1e-3)")) %>%
+  full_join(sim_q_nc_hi %>%
+              mutate(q_nc = "high (1.1e-1)"))
 
-# invades
-V0_b <- V0_c <- V0_init
-pav_inv_params <- param_df %>%
-  mutate(first_virus = "RPV") %>%
-  mutate(pav_conc = pmap(., function(param_foc, param_val, first_virus) param_fun(params_def, param_foc, param_val, first_virus, max_days))) %>%
-  unnest(cols = c(pav_conc)) %>%
-  mutate(nutrient = fct_relevel(nutrient, "low", "+N", "+P"))
-
-# figures
-ggplot(pav_only_params, 
-       aes(x = param_val, y = virus_conc, color = nutrient, linetype = nutrient)) +
+sim_q_nc %>%
+  filter(variable2 == "RPV_pop") %>%
+  ggplot(aes(time, log10(value + 1), 
+             color = q_nc, size = lim_nut_H)) +
   geom_line() +
-  facet_rep_wrap(~ param_foc, scales = "free_x") +
+  scale_color_viridis_d(option = "cividis") +
+  scale_size_manual(values = c(1.2, 0.6)) +
+  facet_wrap(~ nutrient) +
   fig_theme
 
-ggplot(pav_inv_params, 
-       aes(x = param_val, y = virus_conc, color = nutrient, linetype = nutrient)) +
+
+#### q-z interaction effect on Q ####
+
+# values for z and q
+z_nc_vals <- 10^seq(-18, 0, length.out = 10) # same values as above
+q_nc_vals3 <- 10^seq(-5, -1, length.out = 10)
+
+# data frame
+qz_nc_in2 <- tibble(param_foc1 = "z_nc",
+                   param_val1 = z_nc_vals) %>%
+  expand_grid(tibble(param_foc2 = "q_nc",
+                     param_val2 = q_nc_vals3))
+
+# RPV by itself
+rpv_res_qz_nc <- qz_nc_in2 %>%
+  mutate(sim_out = pmap(., function(param_foc1, param_val1, param_foc2, param_val2) param_fun(params_def2, param_foc1, param_val1, param_foc2, param_val2, first_virus = "RPV", V0_b = 0))) %>%
+  unnest(cols = c(sim_out))
+
+# edit dataframe
+rpv_res_qz_nc2 <- rpv_res_qz_nc %>%
+  mutate(z_nc_10 = log10(param_val1),
+         z_nc_f = as.factor(z_nc_10),
+         Q_10 = if_else(variable2 %in% "Q_n", log10(value + 1e-6), NA_real_))
+
+# missing values
+rpv_res_qz_nc2 %>%
+  filter(variable2 == "Q_n" & is.na(value))
+# z = 1e-6, q = 1e-5
+
+rpv_res_qz_nc2 %>%
+  filter(variable2 == "Q_n" & is.na(Q_10))
+# some values are large and negative
+#### start here: model needs to be fixed to prevent negative Q values ####
+
+# figure
+rpv_res_qz_nc2 %>%
+  filter(variable2 == "Q_n") %>%
+  ggplot(aes(x = param_val2, y = value, color = z_nc_10, group = z_nc_f)) +
   geom_line() +
-  facet_rep_wrap(~ param_foc, scales = "free_x") +
-  fig_theme
-# when RPV has a lower q for the non-limiting nutrient, PAV concentration is much lower (q_nc)
-
-
-#### RPV initial simulations ####
-
-# alone
-V0_c <- V0_init
-V0_b <- 0
-rpv_only_params <- param_df %>%
-  mutate(first_virus = "PAV") %>%
-  mutate(pav_conc = pmap(., function(param_foc, param_val, first_virus) param_fun(params_def, param_foc, param_val, first_virus, max_days))) %>%
-  unnest(cols = c(pav_conc)) %>%
-  mutate(nutrient = fct_relevel(nutrient, "low", "+N", "+P"))
-
-# invades
-V0_b <- V0_c <- V0_init
-rpv_inv_params <- param_df %>%
-  mutate(first_virus = "PAV") %>%
-  mutate(pav_conc = pmap(., function(param_foc, param_val, first_virus) param_fun(params_def, param_foc, param_val, first_virus, max_days))) %>%
-  unnest(cols = c(pav_conc)) %>%
-  mutate(nutrient = fct_relevel(nutrient, "low", "+N", "+P"))
-
-# figures
-ggplot(rpv_only_params, 
-       aes(x = param_val, y = virus_conc, color = nutrient, linetype = nutrient)) +
-  geom_line() +
-  facet_rep_wrap(~ param_foc, scales = "free_x") +
-  fig_theme
-
-ggplot(rpv_inv_params, 
-       aes(x = param_val, y = virus_conc, color = nutrient, linetype = nutrient)) +
-  geom_line() +
-  facet_rep_wrap(~ param_foc, scales = "free_x") +
-  fig_theme
-# don't see the same q effect here, but range may be too limited
-
-
-#### q simulation settings ####
-
-max_days <- 100
-
-n <- 7
-
-param_q_pav <- tibble(param_foc = rep(c("q_nc", "q_pc"), each = n),
-                      param_val = c(1.1e-9, 1.1e-8, 1.1e-7, 1.1e-6, 1.1e-5, 1.1e-4, 1.1e-3,
-                                    7.4e-17, 7.4e-15, 7.4e-13, 7.4e-11, 7.4e-9, 7.4e-7, 7.4e-5))
-
-param_q_rpv <- tibble(param_foc = rep(c("q_nb", "q_pb"), each = n), 
-                      param_val = c(1.1e-15, 1.1e-13, 1.1e-11, 1.1e-9, 1.1e-7, 1.1e-5, 1.1e-3,
-                                    7.4e-17, 7.4e-15, 7.4e-13, 7.4e-11, 7.4e-9, 7.4e-7, 7.4e-5))
-
-
-#### PAV q simulations ####
-
-V0_b <- V0_c <- V0_init
-pav_q_params <- param_q_pav %>%
-  mutate(first_virus = "RPV") %>%
-  mutate(pav_conc = pmap(., function(param_foc, param_val, first_virus) param_fun(params_def, param_foc, param_val, first_virus, max_days))) %>%
-  unnest(cols = c(pav_conc)) %>%
-  mutate(nutrient = fct_relevel(nutrient, "low", "+N", "+P"))
-
-ggplot(pav_q_params, 
-       aes(x = param_val, y = virus_conc, color = nutrient, linetype = nutrient)) +
-  geom_line() +
-  facet_rep_wrap(~ param_foc, scales = "free_x") +
+  facet_wrap(~ nutrient, scales = "free") +
+  scale_color_viridis_c(option = "magma", name = "z_nc") +
   scale_x_log10() +
+  scale_y_log10() +
+  labs(x = "RPV min N", y = "Plant N conc.") +
   fig_theme
-
-# why is PAV sensitive to q_nc?
-params_qnc <- params_def
-
-params_qnc["q_nc"] <- 1.1e-6
-
-pav_inv_q_nc <- sim_param_fun(params_qnc, "RPV", 11, 12, max_days-11-12) %>%
-  sim_dat_form()
-
-ggplot(pav_inv_q_nc, aes(x = time, y = value, color = nutrient, linetype = nutrient)) +
-  geom_line() +
-  facet_rep_wrap(~ variable, scales = "free_y") +
-  fig_theme
-# RPV explodes and kills the plant, PAV was still able to invade
-  
-
-#### RPV q simulations ####
-
-V0_b <- V0_c <- V0_init
-rpv_q_params <- param_q_rpv %>%
-  mutate(first_virus = "PAV") %>%
-  mutate(pav_conc = pmap(., function(param_foc, param_val, first_virus) param_fun(params_def, param_foc, param_val, first_virus, max_days))) %>%
-  unnest(cols = c(pav_conc)) %>%
-  mutate(nutrient = fct_relevel(nutrient, "low", "+N", "+P"))
-
-ggplot(rpv_q_params, 
-       aes(x = param_val, y = virus_conc, color = nutrient, linetype = nutrient)) +
-  geom_line() +
-  facet_rep_wrap(~ param_foc, scales = "free_x") +
-  scale_x_log10() +
-  fig_theme
-
-
-#### r simulation settings ####
-
-max_days <- 100
-
-n <- 4
-
-param_r_pav <- tibble(param_foc = rep("r_c", n),
-                      param_val = c(1e-2, 1e-1, 1, 10))
-
-param_r_rpv <- tibble(param_foc = rep("r_b", n),
-                      param_val = c(1e-2, 1e-1, 1, 10))
-
-
-#### PAV r simulations ####
-
-V0_b <- V0_c <- V0_init
-pav_r_params <- param_r_pav %>%
-  mutate(first_virus = "RPV") %>%
-  mutate(pav_conc = pmap(., function(param_foc, param_val, first_virus) param_fun(params_def, param_foc, param_val, first_virus, max_days))) %>%
-  unnest(cols = c(pav_conc)) %>%
-  mutate(nutrient = fct_relevel(nutrient, "low", "+N", "+P"))
-
-ggplot(pav_r_params, 
-       aes(x = param_val, y = virus_conc, color = nutrient, linetype = nutrient)) +
-  geom_line() +
-  facet_rep_wrap(~ param_foc, scales = "free_x") +
-  scale_x_log10() +
-  fig_theme
-
-# time series
-params_rc <- params_def
-
-params_rc["r_c"] <- 10
-
-pav_inv_rc <- sim_param_fun(params_rc, "RPV", 11, 12, max_days-11-12) %>%
-  sim_dat_form()
-
-ggplot(pav_inv_rc, aes(x = time, y = value, color = nutrient, linetype = nutrient)) +
-  geom_line() +
-  facet_rep_wrap(~ variable, scales = "free_y") +
-  fig_theme
-# plant is getting smaller, but not dead
-# PAV can't invade (decreasing almost immediately)
-# RPV values are probably unrealistic
-
-ggplot(filter(pav_inv_rc, time <= 25), 
-       aes(x = time, y = value, color = nutrient, linetype = nutrient)) +
-  geom_line() +
-  facet_rep_wrap(~ variable, scales = "free_y") +
-  fig_theme
-
-# look at internal nutrients without viruses
-V0_b <- V0_c <- 0
-no_inv_rc <- sim_param_fun(params_rc, "RPV", 11, 12, max_days-11-12) %>%
-  sim_dat_form()
-ggplot(filter(no_inv_rc, time <= 25), aes(x = time, y = value, color = nutrient, linetype = nutrient)) +
-  geom_line() +
-  facet_rep_wrap(~ variable, scales = "free_y") +
-  fig_theme
-# the fast virus drags down internal nutrients
-# probably similar mechanism to the q simulation above with RPV
-
-
-#### RPV r simulations ####
-
-V0_b <- V0_c <- V0_init
-rpv_r_params <- param_r_rpv %>%
-  mutate(first_virus = "PAV") %>%
-  mutate(pav_conc = pmap(., function(param_foc, param_val, first_virus) param_fun(params_def, param_foc, param_val, first_virus, max_days))) %>%
-  unnest(cols = c(pav_conc)) %>%
-  mutate(nutrient = fct_relevel(nutrient, "low", "+N", "+P"))
-
-ggplot(rpv_r_params, 
-       aes(x = param_val, y = virus_conc, color = nutrient, linetype = nutrient)) +
-  geom_line() +
-  facet_rep_wrap(~ param_foc, scales = "free_x") +
-  scale_x_log10() +
-  fig_theme
-
-# time series
-params_rb <- params_def
-
-params_rb["r_b"] <- 10
-
-rpv_inv_rb <- sim_param_fun(params_rb, "PAV", 11, 12, max_days-11-12) %>%
-  sim_dat_form()
-
-ggplot(rpv_inv_rb, aes(x = time, y = value, color = nutrient, linetype = nutrient)) +
-  geom_line() +
-  facet_rep_wrap(~ variable, scales = "free_y") +
-  fig_theme
-# same as PAV r simulation
-
-
-#### resource supply rate settings ####
-
-max_days <- 100
-
-n <- 6
-
-param_a_lo <- tibble(param_foc = rep(c("a_n_lo", "a_p_lo"), each = n),
-                     param_val = c(1.1e-11, 1.1e-10, 1.1e-9, 1.1e-8, 1.1e-7, 1.1e-6,
-                                   1.6e-12, 1.6e-11, 1.6e-10, 1.6e-9, 1.6e-8, 1.6e-7))
-
-
-#### PAV a simulations ####
-
-V0_b <- V0_c <- V0_init
-pav_a_params <- param_a_lo %>%
-  mutate(first_virus = "RPV") %>%
-  mutate(pav_conc = pmap(., function(param_foc, param_val, first_virus) param_fun(params_def, param_foc, param_val, first_virus, max_days))) %>%
-  unnest(cols = c(pav_conc)) %>%
-  mutate(nutrient = fct_relevel(nutrient, "low", "+N", "+P"))
-
-ggplot(pav_a_params, 
-       aes(x = param_val, y = virus_conc, color = nutrient, linetype = nutrient)) +
-  geom_line() +
-  facet_rep_grid(nutrient ~ param_foc, scales = "free") +
-  scale_x_log10() +
-  fig_theme
-
-# time series
-params_alo <- params_def
-
-params_alo["a_n_lo"] <- 1.1e-13
-
-pav_inv_alo <- sim_param_fun(params_alo, "RPV", 12, 11, max_days-11-12) %>%
-  sim_dat_form()
-
-ggplot(pav_inv_alo, aes(x = time, y = value, color = nutrient, linetype = nutrient)) +
-  geom_line() +
-  facet_rep_wrap(~ variable, scales = "free_y") +
-  fig_theme
-
-# compare to regular supply rates
-pav_inv <- sim_param_fun(params_def, "RPV", 12, 11, max_days-11-12) %>%
-  sim_dat_form()
-
-ggplot(pav_inv, aes(x = time, y = value, color = nutrient, linetype = nutrient)) +
-  geom_line() +
-  facet_rep_wrap(~ variable, scales = "free_y") +
-  fig_theme
-
-#### Start here: double check simulations in ode_simulations ####
-# the above figure doesn't match the manuscript figure and it should
-# error in one of the models?
-# can initial resource amount be changed to be a function of low/high?
-
-
-#### RPV a simulations ####
-
-V0_b <- V0_c <- V0_init
-rpv_a_params <- param_a_lo %>%
-  mutate(first_virus = "PAV") %>%
-  mutate(pav_conc = pmap(., function(param_foc, param_val, first_virus) param_fun(params_def, param_foc, param_val, first_virus, max_days))) %>%
-  unnest(cols = c(pav_conc)) %>%
-  mutate(nutrient = fct_relevel(nutrient, "low", "+N", "+P"))
-
-ggplot(rpv_a_params, 
-       aes(x = param_val, y = virus_conc, color = nutrient, linetype = nutrient)) +
-  geom_line() +
-  facet_rep_grid(nutrient ~ param_foc, scales = "free") +
-  scale_x_log10() +
-  fig_theme
+# consistent with expected trend
