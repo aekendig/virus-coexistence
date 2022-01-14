897ce2573cad492ede7cde18af2494cd2af6fb5d
aekendig <aekendig@gmail.com>
2022-01-14 11:48:18 -0600

    evaluate surveyor experience

--- code/sensitivity_analysis.R
+++ code/sensitivity_analysis.R
@@ -49,7 +49,6 @@ param_fun <- function(params_in, param_foc1, param_val1, param_foc2, param_val2,
   
   # edit output
   mod_out2 <- mod_out %>%
-    filter(time == max(time)) %>%
     mutate(VbH_low = V_b_low * H_low,
            VbH_n = V_b_n * H_n,
            VbH_p = V_b_p * H_p,
@@ -58,7 +57,7 @@ param_fun <- function(params_in, param_foc1, param_val1, param_foc2, param_val2,
            VcH_n = V_c_n * H_n,
            VcH_p = V_c_p * H_p,
            VcH_np = V_c_np * H_np) %>%
-    pivot_longer(cols = everything(),
+    pivot_longer(cols = -time,
                  names_to = "variable",
                  values_to = "value") %>%
     mutate(nutrient = case_when(str_ends(variable, "low") == T ~ "low",
@@ -75,13 +74,26 @@ param_fun <- function(params_in, param_foc1, param_val1, param_foc2, param_val2,
                                  str_starts(variable, "V_c") == T ~ "RPV_conc",
                                  str_starts(variable, "VbH") == T ~ "PAV_pop",
                                  str_starts(variable, "VcH") == T ~ "RPV_pop"),
-           virus_abund = if_else(str_starts(variable, "V") == T, 
-                                 log10(value + 1), NA_real_),
+           value = if_else(str_starts(variable, "V") == T, 
+                                 log10(value + 1), value),
            abund_type = if_else(str_starts(variable, "V") == T, 
                                 str_sub(variable2, 5, 7), NA_character_))
   
+  # save time series
+  print(ggplot(mod_out2, aes(x = time, y = value, 
+                             color = nutrient, linetype = nutrient)) +
+          geom_line() +
+          facet_wrap(~ variable2, scales = "free_y") +
+          scale_color_viridis_d() +
+          fig_theme +
+          labs(title = paste0(param_foc1, " = ", param_val1, "; ", param_foc2, " = ", param_val2)))
+  
+  # last time point
+  mod_out3 <- mod_out2 %>%
+    filter(time == max(time))
+  
   # return output
-  return(mod_out2)
+  return(mod_out3)
 
 }
 
@@ -93,11 +105,15 @@ virus2_model_sim(params_def2, "RPV", V0_b = V0, V0_c = V0,
   filter(time == max(time) & variable2 %in% c("PAV_conc", "PAV_pop")) %>%
   mutate(virus_conc = log10(value + 1))
 
+pdf("output/temp_sensitivity_analysis_fig.pdf")
 param_fun(params_def2, "r_b", 0.0961, "r_c", 0.221, "RPV") %>%
   filter(variable2 %in% c("PAV_conc", "PAV_pop"))
+dev.off()
 
+pdf("output/temp_sensitivity_analysis_fig.pdf")
 param_fun(params_def2, "a_n_lo", 1.1e-10, "r_c", 0.221, "RPV") %>%
   filter(variable2 %in% c("PAV_conc", "PAV_pop"))
+dev.off()
 
 
 #### resource supply rates ####
@@ -344,25 +360,37 @@ sim_q_nc %>%
 #### q-z interaction effect on Q ####
 
 # values for z and q
-z_nc_vals <- 10^seq(-18, 0, length.out = 10) # same values as above
-q_nc_vals3 <- 10^seq(-5, -1, length.out = 10)
+z_nc_vals2 <- 10^(-18:-5)
+q_nc_vals3 <- 10^(-5:-1)
 
 # data frame
 qz_nc_in2 <- tibble(param_foc1 = "z_nc",
-                   param_val1 = z_nc_vals) %>%
+                   param_val1 = z_nc_vals2) %>%
   expand_grid(tibble(param_foc2 = "q_nc",
                      param_val2 = q_nc_vals3))
 
 # RPV by itself
+pdf("output/sensitivity_analysis_qz_nc.pdf")
 rpv_res_qz_nc <- qz_nc_in2 %>%
   mutate(sim_out = pmap(., function(param_foc1, param_val1, param_foc2, param_val2) param_fun(params_def2, param_foc1, param_val1, param_foc2, param_val2, first_virus = "RPV", V0_b = 0))) %>%
   unnest(cols = c(sim_out))
+dev.off()
+# when z_n is low, RPV does better with low q_n and cannot infect with high q_n
+# when z_n is high, RPV kills the plant with low q_n by pulling Q_n down very low, RPV pop then goes to zero
 
 # edit dataframe
 rpv_res_qz_nc2 <- rpv_res_qz_nc %>%
   mutate(z_nc_10 = log10(param_val1),
          z_nc_f = as.factor(z_nc_10),
-         Q_10 = if_else(variable2 %in% "Q_n", log10(value + 1e-6), NA_real_))
+         Q_10 = if_else(variable2 %in% "Q_n", log10(value + 1e-6), NA_real_),
+         plant_alive = if_else(variable2 == "H" & value > 0, 1, 0),
+         RPV_present = if_else(variable2 == "RPV_pop" & value > 0, 1, 0),
+         PAV_present = if_else(variable2 == "PAV_pop" & value > 0, 1, 0)) %>%
+  group_by(param_val1, param_val2, nutrient) %>%
+  mutate(plant_alive = sum(plant_alive, na.rm = T),
+         RPV_present = sum(RPV_present, na.rm = T),
+         PAV_present = sum(PAV_present, na.rm = T)) %>%
+  ungroup()
 
 # missing values
 rpv_res_qz_nc2 %>%
@@ -371,12 +399,11 @@ rpv_res_qz_nc2 %>%
 
 rpv_res_qz_nc2 %>%
   filter(variable2 == "Q_n" & is.na(Q_10))
-# some values are large and negative
-#### start here: model needs to be fixed to prevent negative Q values ####
+# same as above plus z = 1e-5, q = 1e-5 for low (negative Q)
 
 # figure
 rpv_res_qz_nc2 %>%
-  filter(variable2 == "Q_n") %>%
+  filter(variable2 == "Q_n" & plant_alive == 1) %>%
   ggplot(aes(x = param_val2, y = value, color = z_nc_10, group = z_nc_f)) +
   geom_line() +
   facet_wrap(~ nutrient, scales = "free") +
@@ -386,3 +413,34 @@ rpv_res_qz_nc2 %>%
   labs(x = "RPV min N", y = "Plant N conc.") +
   fig_theme
 # consistent with expected trend
+
+# why do some decline at higher RPV min N?
+# looks like z_nc = 1e-6 or 1e-7, q_nc = 1e-1
+# plant-virus competition
+
+# present/absent diagram
+# may be better to do this with longer time series
+rpv_res_qz_nc3 <- rpv_res_qz_nc2 %>%
+  select(param_foc1, param_val1, param_foc2, param_val2, nutrient, plant_alive, RPV_present) %>%
+  unique() %>%
+  mutate(final_present = case_when(plant_alive == 1 & RPV_present == 1 ~ "plant and virus",
+                                   plant_alive == 1 & RPV_present == 0 ~ "plant only",
+                                   plant_alive == 0 & RPV_present == 1 ~ "virus only",
+                                   plant_alive == 0 & RPV_present == 0 ~ "neither"))
+
+ggplot(rpv_res_qz_nc3, aes(x = param_val1, y = param_val2, color = final_present)) +
+  geom_point(size = 10) +
+  facet_wrap(~ nutrient) +
+  scale_color_viridis_d(option = "plasma") +
+  scale_x_log10() +
+  scale_y_log10() +
+  labs(x = "RPV N content", y = "RPV min N") +
+  fig_theme 
+# when q_nc = Q_n,min (1e-3), plant and virus always coexist
+# when q_nc decreases, virus can only persist if it's N content isn't too high (kills plant)
+# when q_nc increases, virus can only persist if it's N content is high (controls plant's N content)
+# all are "neither" for a simulation that crashed (no values after a certain time point)
+# plant and virus present in low bottom right is from a weird simulation (probably an error)
+
+#### start here ####
+# build off above to include virus competition
